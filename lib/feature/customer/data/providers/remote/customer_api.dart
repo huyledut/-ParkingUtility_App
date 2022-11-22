c470dce1b1105fc41c87d04425e0feda05fe0d22
Tran-Nguyen-Anh-Trinh <trinhtranthanhxuyen@gmail.com>
2022-11-22 21:47:03 +0700

    [DUTPU-11] Implement Profile Logic

--- lib/feature/customer/data/providers/remote/customer_api.dart
+++ lib/feature/customer/data/providers/remote/customer_api.dart
@@ -2,6 +2,7 @@ import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:dio/dio.dart';
 import 'package:dut_packing_utility/feature/customer/data/models/customer_model.dart';
 import 'package:dut_packing_utility/feature/customer/data/models/faculties_model.dart';
+import 'package:dut_packing_utility/feature/customer/data/providers/remote/request/customer_update_request.dart';
 import 'package:retrofit/http.dart';
 
 part 'customer_api.g.dart';
@@ -15,4 +16,7 @@ abstract class CustomerAPI {
 
   @GET('/Common/Faculties')
   Future<List<FacultyModel>> getAllFaculty();
+
+  @POST('/Profile')
+  Future<void> customerUpdate(@Body() CustomerUpdateRequest request);
 }
--- lib/feature/customer/domain/repositoties/customer_repo.dart
+++ lib/feature/customer/domain/repositoties/customer_repo.dart
@@ -1,7 +1,9 @@
 import 'package:dut_packing_utility/feature/customer/data/models/customer_model.dart';
 import 'package:dut_packing_utility/feature/customer/data/models/faculties_model.dart';
+import 'package:dut_packing_utility/feature/customer/data/providers/remote/request/customer_update_request.dart';
 
 abstract class CustomerRepo {
   Future<CustomerModel> customerInfo();
   Future<List<FacultyModel>> getAllFaculty();
+  Future<void> customerUpdate(CustomerUpdateRequest request);
 }
