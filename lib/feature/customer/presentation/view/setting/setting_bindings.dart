ce5d7b2d5bde04a66b34772a2e56834ed6b2f572
Tran-Nguyen-Anh-Trinh <trinhtranthanhxuyen@gmail.com>
2022-11-22 23:48:21 +0700

    [DUTPU-14] [App] Implement Logout Logic

--- lib/feature/customer/presentation/view/setting/setting_bindings.dart
+++ lib/feature/customer/presentation/view/setting/setting_bindings.dart
@@ -4,6 +4,6 @@ import '../../controller/setting/setting_controller.dart';
 class SettingBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put(SettingController());
+    Get.put(SettingController(Get.find()));
   }
 }
